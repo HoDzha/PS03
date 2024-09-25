14b8712ef4cf3f03fa5af2a2cfbe07a684a45aa3
Ho Dzha <hodzha@gmail.com>
2024-09-25 11:22:40 +0500

    готово

--- PS03.HW.py
+++ PS03.HW.py
@@ -12,21 +12,25 @@ def get_words():
         # Создаём объект Soup
         soup = BeautifulSoup(response.content, "html.parser")
         # Получаем слово. text.strip удаляет все пробелы из результата
-        english_words = soup.find("div", id="random_word").text.strip()
-        words = translator.translate(english_words, dest='ru').text
-
+        english_word = soup.find("div", id="random_word").text.strip()
+      #   print(f'Оригинал {english_word}')
+        translated_word = translator.translate(english_word, dest='ru').text
+        #   print(f'Перевод {translated_word}')
         # Получаем описание слова
         english_word_definition = soup.find("div", id="random_word_definition").text.strip()
-        word_definition = translator.translate(english_word_definition, dest='ru').text
-
+        translated_word_definition = translator.translate(english_word_definition, dest='ru').text
+        # print(f"Оригинал значение слова {english_word_definition}")
+        # print(f"Перевод значения слова {translated_word_definition}")
         # Чтобы программа возвращала словарь
         return {
-            "words": words,
-            "word_definition": word_definition
+            "translated_word": translated_word,  # Переведенное слово
+            "translated_word_definition": translated_word_definition,  # Переведённое определение
+            "original_word": english_word  # Оригинальное слово
         }
     # Функция, которая сообщит об ошибке, но не остановит программу
-    except:
-        print("Произошла ошибка")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return None
 
 
 # Создаём функцию, которая будет делать саму игру
@@ -35,20 +39,25 @@ def word_game():
     while True:
         # Создаём функцию, чтобы использовать результат функции-словаря
         word_dict = get_words()
-        word = word_dict.get("words")
-        word_definition = word_dict.get("word_definition")
+        if word_dict is None:
+            print("Не удалось получить данные. Попробуйте позже.")
+            break
+        translated_word = word_dict.get("translated_word")
+        translated_word_definition = word_dict.get("translated_word_definition")
 
         # Начинаем игру
-        print(f"Значение слова - {word_definition}")
-        user = input("Что это за слово? ")
-        if user == word:
+        print(f"Значение слова: {translated_word_definition}")
+        user_input = input("Что это за слово (на русском)? ")
+
+        # Сравниваем введённое пользователем слово с переводом
+        if user_input.strip().lower() == translated_word.strip().lower():
             print("Все верно!")
         else:
-            print(f"Ответ неверный, было загадано это слово - {word}")
+            print(f"Ответ неверный, правильное слово было: {translated_word}")
 
-        # Создаём возможность закончить игру
-        play_again = input("Хотите сыграть еще раз? y/n ")
-        if play_again != "y":
+        # Возможность закончить игру
+        play_again = input("Хотите сыграть еще раз? д/н ")
+        if play_again.lower() != "д":
             print("Спасибо за игру!")
             break
 
