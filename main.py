28ffbfc68100d8a70b043e0c132e71f2a589dac6
Ho Dzha <hodzha@gmail.com>
2024-09-25 08:02:26 +0500

    Знакомство с BeautifulSoup

---
+++ main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+url = 'http://quotes.toscrape.com'
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html,'html.parser')
+links = soup.find_all('a')
+# print(links)
+for link in links:
+    print(link.get('href'))
